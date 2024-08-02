76c1686bce0ba999959808d61426de3be0ce76ad
Lucas Vieira <lucasvbs12@gmail.com>
2024-08-02 14:49:55 -0300

    fix: Atualiza rotas de acesso

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ClientComponent } from './client/client.component';
 
 const routes: Routes = [
   {
--- src/app/client/client-routing.module.ts
+++ src/app/client/client-routing.module.ts
@@ -3,13 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { ClientComponent } from './client.component';
 import { AddComponent } from './add/add.component';
 import { EditComponent } from './edit/edit.component';
-import { DeleteComponent } from './delete/delete.component';
 
 const routes: Routes = [
   { path: '', component: ClientComponent },
   { path: 'add', component: AddComponent },
   { path: 'edit/:id', component: EditComponent },
-  { path: 'delete/:id', component: DeleteComponent },
 ];
 
 @NgModule({
