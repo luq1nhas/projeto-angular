e3dd8512327f735504235a62508f308da2efcef5
Lucas Vieira <lucasvbs12@gmail.com>
2024-08-02 01:34:51 -0300

    feat: Adiciona funcionalidade de adicionar cliente

--- src/app/client/add/add.component.ts
+++ src/app/client/add/add.component.ts
@@ -1,10 +1,65 @@
 import { Component } from '@angular/core';
 
+
+interface Value {
+  value: string;
+  view_value: string;
+}
+
 @Component({
   selector: 'app-add',
   templateUrl: './add.component.html',
-  styleUrls: ['./add.component.css']
+  styleUrls: ['./add.component.css'],
 })
 export class AddComponent {
+  id: number = 0;
+  registerType: string = '';
+  personType: string = '';
+  clientName: string = '';
+  clientCPF_CNPJ: string = '';
+  status: string = '';
+  clientType: string = '';
+  clientFantasy: string = '';
+  alterName: string = '';
+  clientRG: string = '';
+  clientPhone: string = '';
+  clientCellphone: string = '';
+  autoDiscount: number = 0;
+  cep: string = '';
+  uf: string = '';
+  country: string = '';
+  adress: string = '';
+  neighborhood: string = '';
+  number: string = '';
+  ie: string = '';
+  description: string = '';
 
+  registersType: Value[] = [
+    { value: 'Cliente', view_value: 'Cliente' },
+    { value: 'Fornecedor', view_value: 'Fornecedor' },
+    { value: 'Cliente-fornecedor', view_value: 'Cliente / Fornecedor' },
+    { value: 'Transportadora', view_value: 'Transportadora' },
+    { value: 'Funcionario', view_value: 'Funcionário' },
+  ];
+
+  peopleType: Value[] = [
+    { value: 'Fisica', view_value: 'Física' },
+    { value: 'Juridica', view_value: 'Jurídica' },
+  ];
+
+  viewType: Value[] = [
+    { value: 'true', view_value: 'Sim' },
+    { value: 'false', view_value: 'Não' },
+  ];
+
+  alterNameType: Value[] = [
+    { value: 'true', view_value: 'Sim' },
+    { value: 'false', view_value: 'Não' },
+  ];
+
+  clientsType: Value[] = [
+    { value: '1', view_value: 'Contribuinte' },
+    { value: '2', view_value: 'Não Contribuinte' },
+    { value: '3', view_value: 'Produtor Rural' },
+  ];
 }
